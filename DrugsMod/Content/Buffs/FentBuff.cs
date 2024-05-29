a90abd50a3dc6977609f37439dd18721b7022c06
ZackAttacisurboi <Zackattacisurboi@gmail.com>
2024-05-29 05:38:51 -0400

    Add files via upload

---
+++ DrugsMod/Content/Buffs/FentBuff.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using Terraria;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace DrugsMod.Content.Buffs
+{
+    public class FentBuff : ModBuff
+    {
+        public static readonly int DefenseBonus = 45;
+        public static readonly int Life = 1000;
+        public override void SetStaticDefaults()
+        {
+            Main.debuff[Type] = true;  // Is it a debuff?
+        }
+        public override void Update(Player player, ref int buffIndex)
+        {
+            player.statDefense += DefenseBonus;
+            player.statLifeMax2 /= 2;
+            player.lifeRegen = 0;
+            player.lifeRegenTime = 0;
+            player.maxRunSpeed = 1.2f;
+        }
+    }
+}
---
+++ DrugsMod/Content/Items/FentPill.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.ComponentModel;
+using Terraria;
+using Microsoft.Xna.Framework;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.DataStructures;
+
+namespace DrugsMod.Content.Items
+{
+    // This is a basic item template.
+    // Please see tModLoader's ExampleMod for every other example:
+    // https://github.com/tModLoader/tModLoader/tree/stable/ExampleMod
+    public class FentPill : ModItem
+    {
+        // The Display Name and Tooltip of this item can be edited in the 'Localization/en-US_Mods.DrugsMod.hjson' file.
+        public override void SetDefaults()
+        {
+            Item.width = 7;
+            Item.height = 7;
+            Item.UseSound = SoundID.Item2;
+            Item.useStyle = ItemUseStyleID.EatFood;
+            Item.useTime = 40;
+            Item.useAnimation = 40;
+            Item.consumable = true;
+            Item.maxStack = 9999;
+            Item.buffType = ModContent.BuffType<Buffs.FentBuff>();
+            Item.buffTime = 2000;
+        }
+
+        public override bool? UseItem(Player player)
+        {
+            // Hurt the player for 10 points
+            player.Hurt(PlayerDeathReason.ByCustomReason(player.name + " took too much fentanyl."), 50, 0, false, true);
+
+            return true;
+        }
+
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ItemID.DirtBlock, 10);
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.Register();
+        }
+    }
+}
